24dffd52ac21d8e9d886a3013bc353bb5fc8d703
David Bajomo <bajomodavid18@gmail.com>
2023-08-18 14:41:21 +0100

    sidebar navlinks added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Divider from './Divider'
 import { navLinks } from '@/testData'
 import NavLink from './NavLink'
 
 const Sidebar = () => {
+
+  const [selected, setSelected] = useState("Analytics Dashboard")
+
+  const handleSelected = (str) => {
+    setSelected(str)
+  }
+
   return (
     <div>
-        <div className='absolute -right-[250px] md:static top-0 max-w-[200px] min-w-[250px] min-h-screen py-4 px-7'>
+        <div className='absolute -right-[250px] md:static top-0 max-w-[200px] min-w-[270px] min-h-screen py-4 px-7'>
 
           <div className='py-6'>
             <h1 className='text-4xl font-bold tracking-tight'>Paybox</h1>
@@ -15,13 +22,28 @@ const Sidebar = () => {
 
           <Divider/>
 
-          <div className='w-full h-max'>
           {
             navLinks.slice(0,2).map((item) => {
-              <NavLink item={item}/>
+              return <NavLink handleSelected={handleSelected} item={item}/>
             })
           }
-          </div>
+
+          <Divider/>
+
+          {
+            navLinks.slice(2,7).map((item) => {
+              return <NavLink item={item}/>
+            })
+          }
+
+          <Divider/>
+
+          {
+            navLinks.slice(7,11).map((item) => {
+              return <NavLink item={item}/>
+            })
+          }
+          
         </div>
     </div>
   )
