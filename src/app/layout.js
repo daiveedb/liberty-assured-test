5cddb6ef185152a0f2eef3fb254a8825e2e88a5c
David Bajomo <bajomodavid18@gmail.com>
2023-08-20 23:47:02 +0100

    changed icon and name

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link href="https://api.fontshare.com/v2/css?f[]=clash-display@700,300&display=swap" rel="stylesheet"></link>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <title>Liberty Assured Test</title>
       </head>
         <body className={inter.className}>
           <div className='flex justify-between items-center bg-libertyDarkBlue w-[100vw] overflow-x-hidden'>
